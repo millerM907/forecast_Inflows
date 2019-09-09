a483c29ee22b25f352078eed490ba5495c2be495
anyta <anytat@inbox.ru>
2019-09-09 16:13:39 +0300

    Загружены две одинаковые картинки в res/drawable разными способами. Изменен фон для FrameLayout. + ImageView в FrameLayout. Изменены настройки темы и основные цвета res/values/colors.xml (пока для пробы).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView imageView = findViewById(R.id.image_iv);
+        imageView.setBackgroundResource(R.drawable.Highlights);
+
     }
 }
