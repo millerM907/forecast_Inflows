33037e9b684c0a0659363298c6147392966e7296
millerM907 <prilivy@mail.ru>
2019-09-10 10:50:07 +1100

    1. Удален тестовый рисунок sport. 2. Удалена тестовая переменная plList из класса MainActivity.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,21 +3,17 @@ package com.example.myapplication;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView plList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView imageView = findViewById(R.id.image_iv);
-        imageView.setBackgroundResource(R.drawable.Highlights);
 
     }
 }
