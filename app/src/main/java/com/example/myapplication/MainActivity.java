099deb33f0ee891a20d07822ff0c7dbe6f55df6b
millerM907 <prilivy@mail.ru>
2019-10-18 14:23:25 +1100

    1. В класс DataTask добавлен код для автоматической установки картики, ассоциирующейся со значением процентов. 2. В класс DataTaskTwo добавлен код, устанавливающий значение направления ветра на главную страницу ("Текущее").

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,13 +22,13 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_waterTime_1_2;
     TextView tv_waterHeight_4_2;
     TextView tv_windStrength_6_2;
+    TextView tv_windSide_3_2;
 
     TextView tv_temperature_2_2;
     TextView tv_humidity_5_2;
 
     private TextView tvState;
     TextView tv_time;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,27 +45,15 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView1 = findViewById(R.id.iv_sand);
         imageView1.setBackgroundResource(R.drawable.sand1);
 
-        int percent = 86;
-        ImageView imageView2 = findViewById(R.id.iv_wave);
-        imageView2.setBackgroundResource(resourseID.getSearchImageResourseID(percent));
-
         //установка текущего времени города Магадан
         LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("Asia/Magadan"));
         String currentTime = localDateTime.format(DateTimeFormatter.ofPattern("HH:mm"));
         tv_time = findViewById(R.id.tv_time);
         tv_time.setText(currentTime);
 
-        /*
-        //в percent вернули %
-        String percent = "10"; //String.valueOf(TimePercent.calculatePercentUntilEndCycle(tidesForFishingParserList.get(6), tidesForFishingParserList.get(2), tidesForFishingParserList.get(4)));
-        tvPercentTide = findViewById(R.id.tv_percentTide);
-        tvPercentTide.setText(percent);
-        Typeface typefaceCopperplateGothic = Typeface.createFromAsset(getAssets(), "fonts/COPRGTL.TTF");
-        tvPercentTide.setTypeface(typefaceCopperplateGothic);*/
-
-        //запускаем поток по отрисовке процентов
+        //запускаем поток по отрисовке процентов и передаем в него пременную типа ResourseID
         DataTask dataTask = new DataTask();
-        dataTask.execute();
+        dataTask.execute(resourseID);
 
         //запускаем второй поток
         DataTaskTwo dataTaskTwo = new DataTaskTwo();
@@ -73,18 +61,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class DataTask extends AsyncTask<List, Void, List> {
+    class DataTask extends AsyncTask<ResourseID, Void, Object[]> {
 
         @Override
-        protected List doInBackground(List... unused) {
-            return TidesForFishingParser.getTidesForFishingDataList();
+        protected Object[] doInBackground(ResourseID... resourseID) {
+            return new Object[]{TidesForFishingParser.getTidesForFishingDataList(), resourseID};
         }
 
         @SuppressLint("SetTextI18n")
         @Override
-        protected void onPostExecute(List tidesForFishingParserLis) {
-            //super.onPostExecute(tidesForFishingParserLis);
-            List<String> tidesForFishingParserList = tidesForFishingParserLis;
+        protected void onPostExecute(Object[] objectsArray) {
+            List<String> tidesForFishingParserList = (List<String>) objectsArray[0];
+            ResourseID[] resourseID = (ResourseID[]) objectsArray[1];
 
             //установка процента
             String percent = String.valueOf(TimePercent.calculatePercentUntilEndCycle(tidesForFishingParserList.get(6), tidesForFishingParserList.get(2), tidesForFishingParserList.get(4)));
@@ -92,6 +80,10 @@ public class MainActivity extends AppCompatActivity {
             tvPercentTide.setText(getString(R.string.ma_percent, percent));
             Typeface typefaceCopperplateGothic = Typeface.createFromAsset(getAssets(), "fonts/COPRGTL.TTF");
             tvPercentTide.setTypeface(typefaceCopperplateGothic);
+
+            //установка картинки
+            ImageView imageView2 = findViewById(R.id.iv_wave);
+            imageView2.setBackgroundResource(resourseID[0].getSearchImageResourseID(Integer.valueOf(percent)));
 
             //выводим состояние прилив/отлив
             String[] state = new String[]{"ЗАГРУЗКА...", "ПРИЛИВ", "ОТЛИВ", "Полная вода", "Малая вода"};
@@ -102,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                 tv_waterTime_1_1.setText(state[3]);
             } else if (tidesForFishingParserList.get(6).equals("false")){
                 tvState.setText(state[2]);
-                tv_waterTime_1_1.setText(state[3]);
+                tv_waterTime_1_1.setText(state[4]);
             } else {
                 tvState.setText(state[0]);
             }
@@ -132,17 +124,25 @@ public class MainActivity extends AppCompatActivity {
             List<String> forecaParserList = (List<String>) objectsArray[0];
             List<String> gismeteoParserList = (List<String>) objectsArray[1];
 
+            //устанавливаем температуру
             String temperature =  WeatherAverages.calculationTemperatureAverages(forecaParserList.get(0), gismeteoParserList.get(0));
             tv_temperature_2_2 = findViewById(R.id.tv_temperature_2_2);
             tv_temperature_2_2.setText(getString(R.string.ma_temperature, temperature));
 
+            //устанавливаем влажность
             String humidity = WeatherAverages.сalculationAverageHumidity(forecaParserList.get(1), gismeteoParserList.get(3));
             tv_humidity_5_2 = findViewById(R.id.tv_humidity_5_2);
             tv_humidity_5_2.setText(getString(R.string.ma_humidity, humidity));
 
+            //устанавливаем силу ветра
             String windStrength = gismeteoParserList.get(1);
             tv_windStrength_6_2 = findViewById(R.id.tv_windStrength_6_2);
             tv_windStrength_6_2.setText(getString(R.string.ma_wind, windStrength));
+
+            //направление ветра
+            String windDirection = gismeteoParserList.get(2);
+            tv_windSide_3_2 = findViewById(R.id.tv_windSide_3_2);
+            tv_windSide_3_2.setText("Восточный");
         }
 
     }
