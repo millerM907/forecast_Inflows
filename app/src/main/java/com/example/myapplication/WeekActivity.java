d20052fa8e9c8b69f5458c94f9af58b7fd662eef
millerM907 <prilivy@mail.ru>
2020-01-07 03:29:26 +1100

    1. В классе WeekActivity измнен формат вывода даты на кнопки. 2. В случае, если  дата в след месяце, кнопка подсвечивается темным цветом и становится неактивной.

--- app/src/main/java/com/example/myapplication/WeekActivity.java
+++ app/src/main/java/com/example/myapplication/WeekActivity.java
@@ -24,47 +24,155 @@ public class WeekActivity extends Fragment  {
     Button but_day4;
     Button but_day5;
     Button but_day6;
+    Button but_day7;
+    Button but_day8;
+    Button but_day9;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.x, container, false);
+        View v = inflater.inflate(R.layout.activity_week, container, false);
 
         ResourseID resourseID = new ResourseID(getActivity());
 
         String[] dayOfWeek = {"ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"};
+        String point = ",  ";
 
         for(int i = 1; i < 10; i++){
+            String date;
             but_today = v.findViewById(resourseID.but_dayResourseID(i));
             switch (DayOfWeek.from(LocalDate.now(ZoneId.of("Asia/Magadan"))).plus(i)){
                 case MONDAY:
-                    but_today.setText(dayOfWeek[0]);
+                    date = dayOfWeek[0] + point + (LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getDayOfMonth();
+                    but_today.setText(date);
                     break;
 
                 case TUESDAY:
-                    but_today.setText(dayOfWeek[1]);
+                    date = dayOfWeek[1] + point + (LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getDayOfMonth();
+                    but_today.setText(date);
                     break;
 
                 case WEDNESDAY:
-                    but_today.setText(dayOfWeek[2]);
+                    date = dayOfWeek[2] + point + (LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getDayOfMonth();
+                    but_today.setText(date);
                     break;
 
                 case THURSDAY:
-                    but_today.setText(dayOfWeek[3]);
+                    date = dayOfWeek[3] + point + (LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getDayOfMonth();
+                    but_today.setText(date);
                     break;
 
                 case FRIDAY:
-                    but_today.setText(dayOfWeek[4]);
+                    date = dayOfWeek[4] + point + (LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getDayOfMonth();
+                    but_today.setText(date);
                     break;
 
                 case SATURDAY:
-                    but_today.setText(dayOfWeek[5]);
+                    date = dayOfWeek[5] + point + (LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getDayOfMonth();
+                    but_today.setText(date);
                     break;
 
                 case SUNDAY:
-                    but_today.setText(dayOfWeek[6]);
+                    date = dayOfWeek[6] + point + (LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getDayOfMonth();
+                    but_today.setText(date);
                     break;
             }
 
+        }
+
+        String[] monthOfYear = {"  янв", "  фев", "  мар", "  апр", "  май", "  июнь", "  июль", "  авг", "  сен", "  окт", "  нояб", "  дек"};
+
+
+        String currentMonth = LocalDate.now(ZoneId.of("Asia/Magadan")).getMonth().toString();
+
+        for(int i = 1; i < 10; i++){
+            String date;
+            but_today = v.findViewById(resourseID.but_dayResourseID(i));
+            switch ((LocalDate.now(ZoneId.of("Asia/Magadan"))).plusDays(i).getMonth()){
+                case JANUARY:
+                   if(!currentMonth.equals("JANUARY")){
+                       but_today.setBackgroundResource(R.drawable.my_bottom2);
+                   }
+                   date = but_today.getText() + monthOfYear[0];
+                   but_today.setText(date);
+                   break;
+                case FEBRUARY:
+                    if(!currentMonth.equals("FEBRUARY")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[1];
+                    but_today.setText(date);
+                    break;
+                case MARCH:
+                    if(!currentMonth.equals("MARCH")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[2];
+                    but_today.setText(date);
+                    break;
+                case APRIL:
+                    if(!currentMonth.equals("APRIL")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[3];
+                    but_today.setText(date);
+                    break;
+                case MAY:
+                    if(!currentMonth.equals("MAY")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[4];
+                    but_today.setText(date);
+                    break;
+                case JUNE:
+                    if(!currentMonth.equals("JUNE")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[5];
+                    but_today.setText(date);
+                    break;
+                case JULY:
+                    if(!currentMonth.equals("JULY")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[6];
+                    but_today.setText(date);
+                    break;
+                case AUGUST:
+                    if(!currentMonth.equals("AUGUST")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[7];
+                    but_today.setText(date);
+                    break;
+                case SEPTEMBER:
+                    if(!currentMonth.equals("SEPTEMBER")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[8];
+                    but_today.setText(date);
+                    break;
+                case OCTOBER:
+                    if(!currentMonth.equals("OCTOBER")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[9];
+                    but_today.setText(date);
+                    break;
+                case NOVEMBER:
+                    if(!currentMonth.equals("NOVEMBER")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[10];
+                    but_today.setText(date);
+                    break;
+                case DECEMBER:
+                    if(!currentMonth.equals("DECEMBER")){
+                        but_today.setBackgroundResource(R.drawable.my_bottom2);
+                    }
+                    date = but_today.getText() + monthOfYear[11];
+                    but_today.setText(date);
+                    break;
+            }
         }
 
         but_today = v.findViewById(R.id.but_today);
@@ -74,6 +182,9 @@ public class WeekActivity extends Fragment  {
         but_day4 = v.findViewById(R.id.but_day4);
         but_day5 = v.findViewById(R.id.but_day5);
         but_day6 = v.findViewById(R.id.but_day6);
+        but_day7 = v.findViewById(R.id.but_day7);
+        but_day8 = v.findViewById(R.id.but_day8);
+        but_day9 = v.findViewById(R.id.but_day9);
 
         but_today.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,6 +244,33 @@ public class WeekActivity extends Fragment  {
             public void onClick(View v) {
                 if(getEqualsMonth(6)){
                     startIntent(6, but_day6.getText().toString());
+                }
+            }
+        });
+
+        but_day7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getEqualsMonth(7)){
+                    startIntent(7, but_day7.getText().toString());
+                }
+            }
+        });
+
+        but_day8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getEqualsMonth(8)){
+                    startIntent(8, but_day8.getText().toString());
+                }
+            }
+        });
+
+        but_day9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getEqualsMonth(9)){
+                    startIntent(9, but_day9.getText().toString());
                 }
             }
         });
